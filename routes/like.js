386d1f672e6700489d5136052c6cb91b21cd98bd
Mahip Adhikari <mahip.adhikari4@gmail.com>
2022-08-26 20:22:20 +0545

    test

--- routes/like.js
+++ routes/like.js
@@ -2,6 +2,7 @@ const express= require("express");
 const router= express.Router();
 const User = require("../models/userSchema");
 const Chat = require("../models/chatSchema");
+const { clear } = require("google-auth-library/build/src/auth/envDetect");
 
 
 router.post("/like", async(req,res)=>{
@@ -10,17 +11,35 @@ router.post("/like", async(req,res)=>{
 
     Chat.findOne({_id:_id}, async(err,foundChat)=>{
         if(foundChat){
-          Chat.findOne({userId:userId},async(err, foundUser)=>{
-            if(foundUser){
-                foundChat.like--;
-            }
-            else{
-                foundChat.userId.push(userId);
-                foundChat.like++;
-                foundChat.save();
+            const num= foundChat.userId.length;
+           
+            for(let i=0; i<num; i++){
+                if(foundChat.userId[i]===userId){
+                   
+                    Chat.updateOne({_id:_id},{$pull:{userId:`${userId}`}},(err,result)=>{
+                        if(err){
+                          console.log(err);
+                        }
+                        else{
+                            console.log(result);
+                        }
+                    });
+                   
+                   
+                }
+                else{
                 
+                    Chat.updateOne({_id:_id},{$push:{userId:[`${userId}`]}},(err,result)=>{
+                        if(err){
+                            console.log(err);
+                        }
+                        else{
+                            console.log(result);
+                        }
+                    });
+                }
+               
             }
-          })  
          
     }
 })
